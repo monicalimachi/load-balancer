9db13432208b021395bf8aa4f549a18db24564ad
monicalimachi <dolce.monikita@gmail.com>
2022-10-25 07:48:39 -0400

    project

---
+++ app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template, redirect, url_for, request
+import os
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+ 
+ 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+ 
+ 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 3003))
+    app.run(debug=True, host='0.0.0.0')
